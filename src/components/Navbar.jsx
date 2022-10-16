23acd722b6a5ce17643a4177e9a6d7a8f173136b
jordiroca94 <jordirocasoler94@gmail.com>
2022-10-16 21:30:30 +0200

    fix:navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,17 @@ const NavBar = () => {
   return (
     <nav className='flex text-white w-full gap-16 uppercase text-3xl justify-end py-8 px-20'>
         <div>
-            <a href="#about" onClick={()=> setActiveNav("#about")} className={`${activeNav === "#about" ? "active" : ""}`} >
+            <a href="#about" onClick={()=> setActiveNav("#about")} className={`${activeNav === "#about" ? "active" : ""} underlineHover `} >
                 <h5>About me</h5>
             </a>
         </div>
         <div>
-            <a href="#webdev" onClick={()=> setActiveNav("#skills")} className={activeNav === "#skills" ? "active" : ""}>      
+            <a href="#webdev" onClick={()=> setActiveNav("#webdev")} className={`${activeNav === "#webdev" ? "active" : ""} underlineHover`} >    
                 <h5>Web Developer</h5>
             </a>
         </div>
         <div>
-            <a href="#experience" onClick={()=> setActiveNav("#experience")} className={activeNav === '#experience' ? 'active' : ''}>      
+            <a href="#passions" onClick={()=> setActiveNav("#passions")} className={`${activeNav === "#passions" ? "active" : ""} underlineHover`} >  
                 <h5>MY PASSIONS</h5>
             </a>
         </div>
