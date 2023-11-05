3889efc15b590820209f61920f77304a32786711
Jordi Roca Soler <jordirocasoler94@gmail.com>
2023-11-05 20:13:57 +0100

    feat: experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Container from "./Container";
+import Image from "next/image";
+import logoDallonses from "../public/assets/dallonsesLogo.jpeg";
+import logoClowid from "../public/assets/clowIdLogo.png";
 
 const Experience = () => {
   const jobExperiences = [
@@ -11,7 +14,7 @@ const Experience = () => {
         "Front End development of ClowID web app (fintech swedish startup) using Agile methodology.",
       stack:
         "Front-end: HTML5 | SCSS | Tailwind | JavaScript (ES6) | TypeScript | Angular 12.",
-      logo: "",
+      logo: logoClowid,
     },
     {
       key: "2",
@@ -20,19 +23,24 @@ const Experience = () => {
       description: "Front End development of Dallonses",
       stack:
         "Front-end: HTML5 | SCSS | Tailwind | JavaScript (ES6) | TypeScript | Angular 12.",
-      logo: "",
+      logo: logoDallonses,
     },
   ];
   return (
     <Container className="bg-matteBlack text-white">
       <h2 className="flex justify-center font-bold text-4xl">Experience</h2>
-      <div className="flex gap-4 pt-20">
+      <div className="flex justify-center gap-20 pt-20">
         {jobExperiences.map((item) => (
           <div
-            className="flex flex-col border border-white p-6 rounded-lg"
+            className="flex flex-col border border-white p-6 gap-y-2 rounded-lg w-1/3"
             key={item.key}
           >
-            <h3 className="text-4xl text-bold">{item.company}</h3>
+            <Image
+              className="rounded-lg max-h-64 w-full object-cover"
+              src={item.logo}
+              alt="Jordi-Roca"
+            />
+            <h3 className="text-4xl text-bold pt-3">{item.company}</h3>
             <p>{item.date}</p>
             <h4>{item.description}</h4>
             <h5>{item.stack}</h5>
