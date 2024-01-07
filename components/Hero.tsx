2fd3074ae0a30c04bbbc63ba70781012d27fd7cc
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-01-07 01:26:16 +0100

    fix: Keyboard Animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,23 +3,21 @@ import Image from "next/image";
 import React from "react";
 import image from "../public/assets/jordi-roca.png";
 import Container from "./Container";
-import TextAnimation from "./animations/TextAnimation";
 import SimpleAnimation from "./animations/SimpleAnimation";
 import useColor from "@/hooks/useColor";
+import KeyboardAnimation from "./animations/KeyboardAnimation";
 
-const Introduction = () => {
+const Hero = () => {
   const ref = useColor<HTMLDivElement>();
 
   return (
     <Container ref={ref} className="h-screen bg-matteBlack text-white">
-      <div className="flex flex-col justify-center items-center pt-20 lg:pt-10">
-        <TextAnimation>
-          <div className="flex flex-col text-center gap-2">
-            <h3 className="text-lg">Hello I am </h3>
-            <h1 className="text-5xl font-semibold">Jordi Roca</h1>
-            <h3 className="text-2xl">Software Developer </h3>
-          </div>
-        </TextAnimation>
+      <div className="flex flex-col lg:flex-row justify-center lg:justify-around items-center pt-20 lg:pt-32">
+        <KeyboardAnimation
+          className="text-3xl lg:text-6xl text-center lg:text-left"
+          text={["Hello I'am", "Jordi Roca", "Software Developer"]}
+          once
+        />
         <div className="sm:w-1/2 lg:w-1/3 py-10">
           <SimpleAnimation>
             <Image
@@ -34,4 +32,4 @@ const Introduction = () => {
   );
 };
 
-export default Introduction;
+export default Hero;
